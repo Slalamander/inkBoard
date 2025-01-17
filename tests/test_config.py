1d3fad2d156abc4947bb003a4cee7dc8630045ef
Sander <sanderderidder@hotmail.nl>
2025-01-17 17:25:00 +0100

    feat(configuration): :white_check_mark: start implementing tests somewhat

---
+++ tests/test_config.py
@@ -0,0 +1,7 @@
+"Tests for the configuration"
+
+
+def test_screenentry():
+    ##This should test if the screenentry matches the __init__ of the screen
+    ##So the current validation that happens in the file already.
+    return
