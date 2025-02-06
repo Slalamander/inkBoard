16a775ce22f7aa39d2d337ddc7ad2f02167518ae
Sander <sanderderidder@hotmail.nl>
2025-02-06 16:27:10 +0100

    feat(platform): :tada: Create file for platform handlers

---
+++ inkBoard/platforms/handlers.py
@@ -0,0 +1,6 @@
+"""Handlers for certain device features and the like that are not in the base screen
+
+These handlers should be configured under specific config entries
+"""
+
+##At least: add a handler for the Connection Feature to allow for automatic reconnect
