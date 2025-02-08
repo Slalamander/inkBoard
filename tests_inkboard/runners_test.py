43983f567ab194d1f1706064e442f79f6bb5ff17
Sander <sanderderidder@hotmail.nl>
2025-02-08 15:07:34 +0100

    test: ✅ add tests for testing the reload_safe_task

--- tests_inkboard/runners_test.py
+++ tests_inkboard/runners_test.py
@@ -7,19 +7,29 @@ from inkBoard import core as CORE, bootstrap, async_
 
 @pytest.mark.asyncio
 async def test_reload_safe_tasks():
-
+    # print("Testing reload")
     conf = Path(__file__).parent / "configs" / "testconfig.yaml"
     await bootstrap.setup_core(conf)
-    shield_task = async_.create_reload_safe_task(_shield_test())
+    # await bootstrap.start_core(CORE)
+    # await bootstrap.run_core(CORE)
+    shield_task = async_.create_reload_safe_task(_shield_coro(), name="shielded test task")
+    assert async_.is_reload_shielded_task(shield_task), "task is not shielded"
 
     await bootstrap.reload_core(CORE)
-
     assert not shield_task.cancelled(), "reload safe task has been incorrectly cancelled"
     
-    shield_task.cancel("Testing cancellation")
+    await bootstrap.setup_core(conf)
+    await bootstrap.stop_core(CORE)
+    await asyncio.sleep(0.1)
+    assert shield_task.cancelled(), "reload safe task was not cancelled when it should have been (quitting inkBoard)"
 
+    shield_task = async_.create_reload_safe_task(_shield_coro(), name="shielded test task")
+    shield_task.cancel("Cancelling manually")
+    await asyncio.sleep(0.1)
     assert shield_task.cancelled(), "reload safe task was not cancelled when it should have been (manually)"
 
-async def _shield_test():
+
+async def _shield_coro():
     while True:
         await asyncio.sleep(5)
+
