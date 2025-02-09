8282f6773bc6dd5e53cfa348ee888216eeeb3296
Sander <sanderderidder@hotmail.nl>
2025-02-09 12:57:47 +0100

    refactor(tool): ♻️ move function_paramater_dict to util module

--- inkBoard/util.py
+++ inkBoard/util.py
@@ -79,3 +79,70 @@ def reload_full_module(module: Union[str,ModuleType], exclude: list[str] = []):
         sys.modules.pop(mod_name)
     
     return
+
+def function_parameter_dict(func: Callable, types_as_str: bool = False, is_method: bool = False) -> dict[Literal["required", "optional"], dict[str, dict[Literal["type_hint","default"],str]]]:
+    """Returns a dict with function params etc.
+
+    Code here is currently based on low level api. It will be replaced later with inspect. functions
+    Similar to what is used in the docs
+
+    Parameters
+    ----------
+    func : Callable
+        The function to create the parameter dict of
+    types_as_str : bool
+        If True, Any type hints will be converted into a string representation. This means either using the types __name__ if present, otherwise just casting it to a string.
+    is_method : bool
+        If True, the function is considered a method, and the first argument (generally self or cls) will be omitted
+
+    Returns
+    ----------
+    dict:
+        A dict with the keys required and optional, for required parameters and optional parameters.
+        If a parameter has a type hint, it is included as a string in the dict for said parameter under 'type_hint'. For optional parameters, their default values are included as well.
+    """
+    if func.__defaults__:
+        num_defaults = len(func.__defaults__)
+        default_values = func.__defaults__
+    else:
+        num_defaults = 0
+        default_values = []
+
+    f_code = func.__code__
+    num_required = f_code.co_argcount - num_defaults
+
+    func_vars = f_code.co_varnames[:f_code.co_argcount]
+
+    if is_method:
+        req_args = func_vars[1:num_required]
+    else:
+        req_args = func_vars[:num_required]
+
+    opt_args = func_vars[num_required:]
+
+    type_hints = func.__annotations__
+
+
+    required = {}
+    for var_name in req_args:
+        if var_name in type_hints:
+            hint = type_hints[var_name]
+            if types_as_str:
+                hint = getattr(hint,"__name__") if hasattr(hint, "__name__") else str(hint)
+            required[var_name] = {"type_hint": hint}
+        else:
+            required[var_name] = {}
+
+    optional = {}
+    for i, var_name in enumerate(opt_args):
+        optional[var_name] = {"default": default_values[i]}
+
+        if var_name in type_hints:
+            hint = type_hints[var_name]
+            if types_as_str:
+                if types_as_str:
+                    hint = getattr(hint,"__name__") if hasattr(hint, "__name__") else str(hint)
+            optional[var_name].update({"type_hint": hint})
+
+    return {"required": required, "optional": optional}
+
