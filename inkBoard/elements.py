fdb980469feabdd100e5a4daa4dddded39d9185d
Sander <sanderderidder@hotmail.nl>
2025-02-22 18:58:22 +0100

    create elements module

---
+++ inkBoard/elements.py
@@ -0,0 +1,14 @@
+"Elements for inkBoard."
+##This mainly links to the element library from PythonScreenStackManager
+
+from PythonScreenStackManager.elements import *  # noqa: F403
+from PythonScreenStackManager.elements import (
+    baseelements,  # noqa: F401
+    compoundelements, # noqa: F401
+    menuelements, # noqa: F401
+    layoutelements, # noqa: F401
+    deviceelements, # noqa: F401
+    inputelements, # noqa: F401
+    constants # noqa: F401
+)
+from PythonScreenStackManager.elements.baseelements import Element
