7959014361a87d778a5d954f782f6ad4071677b7
Sander <sanderderidder@hotmail.nl>
2025-02-24 17:26:03 +0100

    Add constants for templating

--- inkBoard/configuration/const.py
+++ inkBoard/configuration/const.py
@@ -1,7 +1,11 @@
 "Constants"
 
 import __main__
+from typing import Callable, Literal
 from pathlib import Path
+import yaml
+
+yaml
 
 from ..constants import INKBOARD_FOLDER, CONFIG_FILE_TYPES
 
@@ -16,7 +20,9 @@ REQUIRED_KEYS = (
     "device",
 )
 
+TEMPLATES_KEY = "templates"
 DASHBOARD_KEYS = (
+    TEMPLATES_KEY,
     "elements",
     "layouts",
     "popups",
@@ -24,3 +30,22 @@ DASHBOARD_KEYS = (
     "statusbar"
     )
 "Config keys that indicate they are used to parse the dashboard, which means they will not be processed passed the first node when reading out the config initially."
+
+TEMPLATE_VARIABLE_TAGS = (
+    "!tmp_var",
+    "!tmp_variable",
+    "!template_var",
+    "!template_variable"
+)
+
+TEMPLATE_EXTEND_TAGS = (
+    "!tmp_extend",
+    "!template_extend",
+)
+
+TEMPLATE_TAGS = (
+    *TEMPLATE_VARIABLE_TAGS,
+    *TEMPLATE_EXTEND_TAGS
+)
+
+validatortype = Callable[[Literal["Element_class"],Literal["Requested_type"]],None]
