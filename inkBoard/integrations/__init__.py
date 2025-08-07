7cbd344766fe1b6cb0745831d881907b579dee46
Sander <sanderderidder@hotmail.nl>
2025-08-07 10:40:30 +0200

    style(integration): 💡 Some comments for renaming

--- inkBoard/integrations/__init__.py
+++ inkBoard/integrations/__init__.py
@@ -1,3 +1,10 @@
 """
 This folder can hold integrations for inkBoard.
 """
+
+##Renaming, if in the end wanted:
+# - Integration: integrates to an external service
+# - Extension: Adds features etc.
+# - Component: a reusable thing
+
+##So extension may be a better name in the end?
