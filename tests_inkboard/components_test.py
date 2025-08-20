6a1932b5d9896d9b8b2881f9c94f76c9356289a1
Sander <sanderderidder@hotmail.nl>
2025-08-20 09:27:34 +0200

    test(component): ✅ Add tests to test loading behaviour of components

--- tests_inkboard/components_test.py
+++ tests_inkboard/components_test.py
@@ -2,11 +2,13 @@ from typing import cast, get_args
 import json
 from pathlib import Path
 import sys
+import importlib
+from contextlib import suppress
 
 import pytest
 
 import inkBoard
-from inkBoard import CORE
+from inkBoard import CORE, util
 
 from inkBoard.components import (
     BaseComponent,
@@ -79,9 +81,10 @@ class TestBaseComponent:
             base_component.load_module()
         return
 
-class TestDummyIntegration:
-
+class IntegrationTestBase:
     foldername = "dummy_integration"
+
+class TestDummyIntegration(IntegrationTestBase):
 
     def test_basic_properties(self, dummy_integration: Integration):
 
@@ -106,28 +109,76 @@ class TestDummyIntegration:
         assert not dummy_integration.abstract_component
         assert dummy_integration.custom_component
 
-    def test_load(self, dummy_integration : Integration):
+    @pytest.fixture(scope="class")
+    def dummy_integration(self):
+        f = TEST_INTEGRATIONS_PATH / self.foldername
+        dummy = Integration(f)
+        yield dummy
+        mod = dummy.module
+        return
+
+class TestIntegrationImporting(IntegrationTestBase):
+    
+    
+    def test_load_before_custom(self, import_dummy: Integration):
+
+        with pytest.raises(ImportWarning):
+            import_dummy.load_module()
+        return
+    
+    def test_load_before_custom_with_file_location(self, import_dummy: Integration):
+
+        #For now, this does not work, as custom is not imported
+        #Want to see what happens when it is loaded itself, perhaps?
+        #Or simply not use custom as a name I guess but check what the fromfilename does
+        # n = inspect.getmodulename(import_dummy.location / "__init__.py") #Does not work, simply returns __init__
+        # Idk see what I'm doing for importing platforms or something
+
+        ##Per above comment: try with package name like inkBoard.integrations.custom.{dummy}
+        import_dummy.load_module(load_from_file_location=True)
+        return
+    
+
+    def test_load(self, import_dummy : Integration):
         #When testing this, also think about cleanup probably?
 
         #Probably need to append the name to Path here?
         #I am 100% sure I have done that somewhere -> see init of the config
-
-        folder = Path(__file__).parent / "configs"
-        folder = str(folder)
-        sys.path.insert(0, folder)
-        import custom
-        sys.path.remove(folder)
-        sys.path_importer_cache.pop(folder, None)
-
         #Test how safe this is when run after each other?
         #Also, may want to add a from_location key to the function
-        dummy_integration.load_module()
+        add_custom_to_path()
+        import_dummy.load_module()
+        assert import_dummy.module is not None, "dummy was not imported correctly"
+        return
+    
+    def test_cleanup(self, import_dummy: Integration):
+        
+        add_custom_to_path()
+        import_dummy.load_module()
+        newmod = importlib.import_module(".dummy", import_dummy.module_name)
+
+        mod = import_dummy.module
+        modname = mod.__name__
+        cleanup_imported_component(import_dummy, clean_custom=True)
+
+        assert modname not in sys.modules, "import_dummy was not cleaned up correctly"
+        assert newmod.__name__ not in sys.modules, "submodule of import_dummy was not cleaned up"
+        
+        submods = [m.__name__ in sys.modules for m in util.get_all_submodules(mod)]
+        assert not any(submods), f"Modules from the import dummy were left over {[m.__name__ for m in submods]}"
+
+        assert "custom" not in sys.modules, "custom was not cleaned up correctly"
         return
 
-    @pytest.fixture(scope="class")
-    def dummy_integration(self):
+    @pytest.fixture()
+    def import_dummy(self):
+        #Use this one to test importing; Should clean up after itself
         f = TEST_INTEGRATIONS_PATH / self.foldername
-        return Integration(f)
+        dummy = Integration(f)
+        yield dummy
+
+        cleanup_imported_component(dummy)
+        return
 
 @pytest.fixture
 def raw_config() -> manifestjson:
@@ -144,3 +195,34 @@ def raw_config() -> manifestjson:
 def base_component(raw_config) -> Integration:
 
     return Integration(None, raw_config)
+
+def add_custom_to_path():
+    folder = Path(__file__).parent / "configs"
+    folder = str(folder)
+    sys.path.insert(0, folder)
+    import custom
+    sys.path.remove(folder)
+    sys.path_importer_cache.pop(folder, None)
+    return
+
+def cleanup_imported_component(component : BaseComponent, clean_custom : bool = True):
+    """Cleans up an imported component by removing it from sys.modules
+
+    This also removed the `custom` module from the modules
+
+    Parameters
+    ----------
+    component : BaseComponent
+        The component to clean up
+    """    
+
+    if component.module:
+        mods = util.get_all_submodules(component.module_name)
+        mod_list = [*mods, component.module]
+        for mod in mod_list:
+            # importlib.reload(mod)
+            sys.modules.pop(mod.__name__, None)
+    
+    if clean_custom:
+        sys.modules.pop("custom", None)
+    return
